2604285dd34331b1c4a9f5ae076bfa433a371389
maulanajulham <julhammaulana@gmail.com>
2025-12-09 07:45:42 +0700

    mengubah file global.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './gLobal.css'
+import '@/GLobal.css'
 import 'remixicon/fonts/remixicon.css';
 import App from './App.tsx'
+
+
 import Navbar from './components/Navbar.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer.tsx';
